137333a861a8148bdda10ccd76bfd2d34cff5c48
Chris Charles <ccharles@uoguelph.ca>
2011-11-23 12:02:33 -0500

    Added --generate-sample-archive option.
    Generates a DSpace Simple Archive Format tree using CSV metadata.

--- dspace-csv-import.py
+++ dspace-csv-import.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-import csv
+import csv, os, os.path
 from optparse import OptionParser
 
 try:
     import cStringIO as StringIO
 except ImportError:
     import StringIO
+
+
+class FileExistsError(Exception): pass
 
 
 # Dublin core tags to be put into generated templates
@@ -40,7 +43,7 @@ expected_tags = {
 }
 
 
-def generate_csv_template(tags):
+def generate_csv_template(tags=expected_tags):
     """Return a CSV metadata template based on the tags passed in.
     
     Arguments:
@@ -56,14 +59,38 @@ def generate_csv_template(tags):
     return output.read()
 
 
+def generate_sample_csv_archive(dir, item_count=3):
+    """Generate a sample DSpace Simple Archive Format tree using CSV metadata.
+
+    Arguments:
+    - `dir`: The directory in which to store the archive.
+    """
+    if os.path.exists(dir):
+        raise FileExistsError, dir
+
+    # TODO: Should really try/except here
+    os.mkdir(dir)
+
+    for i in range(1, item_count+1):
+        item_dir = os.path.join(dir, 'item_%03d' % i)
+        os.mkdir(item_dir)
+        metadata = open(os.path.join(item_dir, 'dublin_core.csv'), 'w')
+        metadata.write(generate_csv_template())
+
+
 if __name__ == '__main__':
 
     parser = OptionParser()
     parser.add_option('-t', '--generate-template', action='store_true',
                       dest='generate_template', default=False,
                       help='Output a CSV template to STDOUT')
+    parser.add_option('-a', '--generate-sample-archive', action='store',
+                      dest='archive_name', metavar='DIR',
+                      help='Generate a sample archive in DIR')
     (options, args) = parser.parse_args()
 
 
-    if options.generate_template:
-        print generate_csv_template(expected_tags)
+    if options.archive_name:
+        generate_sample_csv_archive(options.archive_name)
+    elif options.generate_template:
+        print generate_csv_template()
