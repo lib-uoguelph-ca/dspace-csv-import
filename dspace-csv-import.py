80ec3ce27d839ddae4c949542a805d9c111a1705
Chris Charles <ccharles@uoguelph.ca>
2011-11-23 13:34:27 -0500

    Added clean-archive. Currently generates 'contents' for each item.

--- dspace-csv-import.py
+++ dspace-csv-import.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import csv, os, os.path
+import csv, os, os.path, re
 from optparse import OptionParser
 
 try:
@@ -78,6 +78,42 @@ def generate_sample_csv_archive(dir, item_count=3):
         metadata.write(generate_csv_template())
 
 
+def clean_archive(dir):
+    """Create new XML metadata from existing CSV and create contents manifests.
+
+    Arguments:
+    - `dir`: The DSpace Simple Archive Format archive directory
+    """
+    if not os.path.exists(dir) or not os.path.isdir(dir):
+        # TODO: Specify exception
+        raise Exception
+
+    item_pattern = re.compile('(:?%s)?item_\d{3}$' % os.sep)
+
+    # Exclude 'contents', 'dublin_core.xml', 'metadata_[prefix].xml', or
+    # either metadata name format with a csv extension.
+    excluded_files_pattern = re.compile(
+        '^(:?(:?dublin_core|metadata_[^.]+)\.(:?csv|xml)|contents)$'
+    )
+
+    # Don't need to walk the tree--there should only be the archive directory
+    # itself and one level of item subdirectories.
+    for f in os.listdir(dir):
+        item_dir = os.path.join(dir, f)
+        if os.path.isdir(item_dir) and item_pattern.search(item_dir):
+
+            # Generate the contents files
+            contents_file_name = os.path.join(item_dir, 'contents')
+            if not os.path.exists(contents_file_name):
+                contents_file = open(contents_file_name, 'w')
+
+                for f in os.listdir(item_dir):
+                    if not excluded_files_pattern.match(f):
+                        contents_file.write(f + "\n")
+
+                contents_file.close()
+
+
 if __name__ == '__main__':
 
     parser = OptionParser()
@@ -87,10 +123,15 @@ if __name__ == '__main__':
     parser.add_option('-a', '--generate-sample-archive', action='store',
                       dest='archive_name', metavar='DIR',
                       help='Generate a sample archive in DIR')
+    parser.add_option('-c', '--clean-archive', action='store',
+                      dest='clean_archive', metavar='DIR',
+                      help='Convert CSV to XML and create contents files.')
     (options, args) = parser.parse_args()
 
 
-    if options.archive_name:
+    if options.clean_archive:
+        clean_archive(options.clean_archive)
+    elif options.archive_name:
         generate_sample_csv_archive(options.archive_name)
     elif options.generate_template:
         print generate_csv_template()
